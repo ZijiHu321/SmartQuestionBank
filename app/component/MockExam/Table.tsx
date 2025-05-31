54767f22d6e86d89d98728e3c1fc4f50a0bcaa15
chenjk04 <chenjinkai1234444@gmail.com>
2025-05-31 12:10:40 -0400

    mock exam search function update

--- app/component/MockExam/Table.tsx
+++ app/component/MockExam/Table.tsx
@@ -3,39 +3,22 @@ import { useState } from 'react';
 
 import React from 'react';
 
-type PdfItem = {
+
+
+interface PdfItem {
   course: string;
   unit: string;
   examType: string;
-  filename: string; 
-};
+  filename: string;
+}
 
-const pdfData: PdfItem[] = [
-  // cal1 course
-  { course: 'cal1', unit: 'Limits and Continuity', examType: 'Unit Test', filename: 'cal1_Limits and Continuity.pdf' },
-  { course: 'cal1', unit: 'Finding Derivative Functions', examType: 'Unit Test', filename: 'cal1_Finding Derivative Functions.pdf' },
-  { course: 'cal1', unit: 'Properties of Curves', examType: 'Unit Test', filename: 'cal1_Properties of Curves.pdf' },
-  { course: 'cal1', unit: 'Application of Derivatives', examType: 'Unit Test', filename: 'cal1_Application of Derivatives.pdf' },
+interface PdfTableProps {
+  List: PdfItem[];
+  setList: React.Dispatch<React.SetStateAction<PdfItem[]>>;
+}
 
-  // ib_cal course
-  { course: 'ib_cal', unit: 'Limits', examType: 'Unit Test', filename: 'ib_cal_Limits.pdf' },
-  { course: 'ib_cal', unit: 'Finding Derivative Functions', examType: 'Unit Test', filename: 'ib_cal_Finding Derivative Functions.pdf' },
-  { course: 'ib_cal', unit: 'Properties of Curves', examType: 'Unit Test', filename: 'ib_cal_Properties of Curves.pdf' },
-  { course: 'ib_cal', unit: 'Application of Derivatives', examType: 'Unit Test', filename: 'ib_cal_Application of Derivatives.pdf' },
-  { course: 'ib_cal', unit: 'Integral', examType: 'Unit Test', filename: 'ib_cal_Integral.pdf' },
-  { course: 'ib_cal', unit: 'Kinematics', examType: 'Unit Test', filename: 'ib_cal_Kinematics.pdf' },
-  { course: 'ib_cal', unit: 'Taylor Series', examType: 'Unit Test', filename: 'ib_cal_Taylor Series.pdf' },
-  { course: 'ib_cal', unit: 'Differential Equations', examType: 'Unit Test', filename: 'ib_cal_Differential Equations.pdf' },
-
-  // ib_stat course
-  { course: 'ib_stat', unit: 'Probability', examType: 'Unit Test', filename: 'ib_stat_Probability.pdf' },
-  { course: 'ib_stat', unit: 'Statistics', examType: 'Unit Test', filename: 'ib_stat_Statistics.pdf' },
-  { course: 'ib_stat', unit: 'Discrete Random Variables', examType: 'Unit Test', filename: 'ib_stat_Discrete Random Variables.pdf' },
-  { course: 'ib_stat', unit: 'Continuous Random Variables', examType: 'Unit Test', filename: 'ib_stat_Continuous Random Variables.pdf' },
-];
-
-const PdfTable = () => {
-  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
+const PdfTable: React.FC<PdfTableProps> = ({ List, setList }) => {
+  const [hoveredRow, setHoveredRow] = React.useState<number | null>(null);
 
   return (
     <div style={{
@@ -100,7 +83,7 @@ const PdfTable = () => {
             </tr>
           </thead>
           <tbody>
-            {pdfData.map((item, index) => (
+            {List.map((item, index) => (
               <tr 
                 key={index}
                 onMouseEnter={() => setHoveredRow(index)}
