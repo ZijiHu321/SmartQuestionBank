e4e041620161c54aaa2d004ed01b5e2622e27221
ZijiHu321 <zacharyhu321@gmail.com>
2025-08-26 20:49:40 -0300

    Add questions for kkinamatics and taylor IB Calculus

---
+++ app/unit/IBcalculus/Kinematics/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import LALoader from "@/app/LA_Loader";
+
+
+export default function IntegralPage() {
+    return <>
+    <LALoader filePath="LAquestion/IB Calculus/Kin.txt" course="IB Calculus" />
+    
+    </>;
+  }
