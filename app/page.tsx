984e6a45afde35a859cd076b4858867ec3f77409
Haron Wang <Haoran0425@gmail.com>
2025-09-15 22:32:27 -0400

    style

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,32 @@ export default function HomePage() {
         <main>
             <div className="introBox">
                 <h2>Brief intro to the web</h2>
-                <p>This is where you can write a short introduction about the purpose of the site.</p>
+                <p>Hi, warriors!
+
+                    Welcome to your Smart Question Bank – your secret weapon for conquering the exams.
+
+                    Here you can find our original ib style questions and real past paper questions. It's also your personal training ground with smart diagnosis. Every question you practice is analyzed to pinpoint your exact strengths and weaknesses.
+
+                    Forget generic studying. Focus your energy where it matters most, turn weaknesses into strengths, and walk into your exams with unshakable confidence.
+
+                    Your journey to a 45 starts here. Let’s get to work.
+                </p>
             </div>
 
             <div className="thingsBox">
                 <h2>⚠️ Something you need to know...</h2>
                 <ol>
-                    <li>手机端显示 (mobile display support)</li>
-                    <li>点击
+                    <li>Mobile display support</li>
+                    <li>Click on
                         <img
                             src="/profile-user.png"
                             alt="icon"
-                            style={{ width: "17px", height: "17px", verticalAlign: "middle", marginLeft: "5px", 
-                                marginRight: "5px" }}
+                            style={{
+                                width: "17px", height: "17px", verticalAlign: "middle", marginLeft: "5px",
+                                marginRight: "5px"
+                            }}
                         />
-                        可以看到收藏题目和做题分析(data collection & risk analysis)
+                        you can see marked questions and practice analysis
                     </li>
                     <li>Get started by clicking <strong>Chapter</strong></li>
                 </ol>
